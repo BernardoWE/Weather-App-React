a9b81d8b01e31a38c28b139da6ab84e4742ec195
BernardoWE <bernardo.augusto8022@gmail.com>
2026-05-28 22:10:53 -0300

    alterado estilos

--- Weather-app/src/components/style.js
+++ Weather-app/src/components/style.js
@@ -3,9 +3,17 @@ import { Colors } from "../pages/home/style.js";
 export const Card = styled.div`
     background-color: ${Colors.blue800};
     border-radius: 10px;
-    padding: 1rem;
+    padding: 0.7em;
     /* margin: 0.5rem; */
     color: ${Colors.blue0};
-
+    
     flex: 1;
+    p:first-of-type{
+        margin-bottom: 1em;
+        opacity: .6;
+    }
+    strong{
+        font-size: 1.6em;
+        
+    }
 `;
